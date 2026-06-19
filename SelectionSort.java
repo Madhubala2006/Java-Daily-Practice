252e196351ddf2eda9e03eba5498b7657766bb4c
Madhu Bala Sivakumar <madhusivakumar1@gmail.com>
2026-06-19 13:41:28 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+public class SelectionSort {
+
+    static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 4, 1, 2};
+
+        selectionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
